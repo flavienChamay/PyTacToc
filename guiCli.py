2876a32a9f3c107ff8db0ef8907261b63df86900
flavienChamay <flavien.chamay@protonmail.com>
2021-03-04 22:48:51 +0100

    Cleaning code

--- guiCli.py
+++ guiCli.py
@@ -6,99 +6,112 @@ This module implements the CLI of the Tic-Tac-Toe game.  It is also the main pro
 :method: player_vs_dumb_computer()
 :method: player_vs_unbeatable_computer()n
 :method: menu_manager()
+:notes: A computer is always X. First human player is always X, second human player is always O.
 """
+
 from gameBoard import GameBoard
 from player import HumanPlayer, DumbComputerPlayer, UnbeatableComputerPlayer
 import json
 
 
-def human_choose_symbol():
-    """
-    This function demands to the human player what symbol he wants.
-
-    :returns HumanPlayer: A HumanPlayer instance with the choosen symbol.
-    """
-
-    print('Choose the symbol you want:', 'X: for the X player',
-          'O: for the O player', sep='\n')
-    return HumanPlayer(input('Your choice: '))
-
-
-def player_vs_player(gameBoard, playerList):
+def player_vs_player(gameBoard, playerPlay):
     """
     This function manages the game between two human players.
 
+    :param gameBoard GameBoard: The game board.
+    :param playerPlay char: X or O for the symbol of the player.
     :var playerList list: The list of all the players involved.
-    :var game GameBoard: The game board.
     :returns: None.
-    """
-
-    #playerList = [human_choose_symbol(), human_choose_symbol()]
-    gameBoard.play_on_board(playerList)
-
-
-def player_vs_dumb_computer():
-    """
-    This function manages the game between a human player and a dumb computer.
-
-    :var playerList list: The list of all the players involved.
-    :var game GameBoard: The game board.
-    :returns: None.
-    """
-
-    playerList = [DumbComputerPlayer('X'), HumanPlayer('O')]
-    print('Computer is X and you are O.')
-    game = GameBoard()
-    game.play_on_board(playerList)
-
-
-def player_vs_unbeatable_computer():
-    """
-    This function manages the game between a human player and an unbeatable computer.
-
-    :var playerList list: The list of all the players involved.
-    :var game GameBoard: The game board.
-    :returns: None.
-    """
-
-    playerList = [UnbeatableComputerPlayer('X'), HumanPlayer('O')]
-    print('Computer is X and you are O.')
-    game = GameBoard()
-    game.play_on_board(playerList)
-
-
-def load_game():
-    """
-
-    :notes:
-    Structure of a backup file:
-    first line -> grid of the game, listed with X or O separated with empty spaces.
-    second line -> symbol of the player who must play.
-    third line -> type of game: (1) dumb computer vs player (2) unbeatable computer vs player (3) human player vs human player
     """
     try:
-        with open("saveGameTTT.txt") as fileSaveGame:
-            listFile = fileSaveGame.readlines()
-            gridLoaded = json.loads(listFile[0])
-            playerPlay = listFile[1].strip()
-            typeOfPlay = listFile[2].strip()
-
-        # Processing the turn of the appropriate player:
         if playerPlay == 'X':
             playerList = [HumanPlayer('X'), HumanPlayer('O')]
         elif playerPlay == 'O':
             playerList = [HumanPlayer('O'), HumanPlayer('X')]
         else:
             raise IOError('Error in the saved file, wrong type of player!')
-        # Processing the type of game:
+        gameBoard.play_on_board(playerList)
+    except IOError as io:
+        print(io)
+
+
+def player_vs_dumb_computer(gameBoard, playerPlay):
+    """
+    This function manages the game between a human player and a dumb computer.
+
+    :param playerPlay char: X or O for the symbol of the player.
+    :param gameBoard GameBoard: The game board.
+    :var playerList list: The list of all the players involved.
+    :returns: None.
+    """
+
+    try:
+        if playerPlay == 'X':
+            playerList = [DumbComputerPlayer('X'), HumanPlayer('O')]
+        elif playerPlay == 'O':
+            playerList = [HumanPlayer('O'), DumbComputerPlayer('X')]
+        else:
+            raise IOError('Error in the saved file, wrong type of player!')
+        gameBoard.play_on_board(playerList)
+    except IOError as io:
+        print(io)
+
+
+def player_vs_unbeatable_computer(gameBoard, playerPlay):
+    """
+    This function manages the game between a human player and an unbeatable computer.
+
+    :param game GameBoard: The game board.
+    :param playerPlay char:
+    :var playerList list: The list of all the players involved.
+    :returns: None.
+    """
+
+    try:
+        if playerPlay == 'X':
+            playerList = [UnbeatableComputerPlayer('X'), HumanPlayer('O')]
+        elif playerPlay == 'O':
+            playerList = [HumanPlayer('O'), UnbeatableComputerPlayer('X')]
+        else:
+            raise IOError('Error in the saved file, wrong type of player!')
+        gameBoard.play_on_board(playerList)
+    except IOError as io:
+        print(io)
+
+
+def load_game():
+    """
+    This function manages the load mechanism of the game from a file.
+
+    :var listFile list: The content of the file in a list.
+    :var gridLoaded list: The grid loaded from the file.
+    :var playerPlay char: The symbol of the player loaded.
+    :var typeOfPlay int: The type of play of the loaded game.
+    :var game GameBoard: The loaded game.
+    :raises IOError: If the file is missing or if wrong infos are on the file.
+    :returns: None.
+    :notes:
+    Structure of a backup file:
+    first line -> grid of the game, listed with X or O separated with empty spaces.
+    second line -> symbol of the player who must play.
+    third line -> type of game: (1) dumb computer vs player (2) unbeatable computer vs player (3) human player vs human player
+    """
+
+    try:
+        with open("saveGameTTT.txt") as fileSaveGame:
+            listFile = fileSaveGame.readlines()
+            gridLoaded = json.loads(listFile[0])
+            playerPlay = listFile[1].strip()
+            typeOfPlay = listFile[2].strip()
+        game = GameBoard()
+        game.grid = gridLoaded
+        # Processing the type of game and the appropriate player
         if typeOfPlay == '1':
-            pass
+            player_vs_dumb_computer(game, playerPlay)
         elif typeOfPlay == '2':
-            pass
+            player_vs_unbeatable_computer(game, playerPlay)
         elif typeOfPlay == '3':
-            game = GameBoard()
-            game.grid = gridLoaded
-            player_vs_player(game, playerList)
+            player_vs_player(game, playerPlay)
         else:
             raise IOError('Error in the saved file, wrong type of play!')
     except IOError as io:
